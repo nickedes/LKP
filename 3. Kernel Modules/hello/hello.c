ab63e2eabf11cbd44406f67df1de3308d455c5d6
Nikhil Mittal <nkmittal4994@gmail.com>
2017-08-09 01:05:02 +0530

    Kernel module to print hello & bye!

---
+++ 3. Kernel Modules/hello/hello.c
@@ -0,0 +1,14 @@
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/kernel.h> /* Needed for KERN_INFO */
+int init_module(void)
+{
+	printk(KERN_INFO "hello kernel\n");
+	/*
+	* A non 0 return means init_module failed; module can't be loaded.
+	*/
+	return 0;
+}
+void cleanup_module(void)
+{
+	printk(KERN_INFO "bye kernel\n");
+}
